bea0ece16173721bda1d2f8e9640cc0e5c27b66d
jcrowe91 <jcrowe9119@gmail.com>
2022-02-13 15:31:01 -0600

    Finished JS project

--- app.js
+++ app.js
@@ -4,7 +4,15 @@ console.log(
 );
 
 // Exercise 1
-
+const firstName = "Jacob";
+let lastName = "Crowe";
+var age = 30;
 // Exercise 2
-
+let fullName = firstName + "" + lastName;
+console.log(fullName);
 // Exercise 3
+let myStory;
+let city = "Hayden";
+let pasttime = "playing banjo";
+myStory = `\Hello! My name is ${fullName}. I live in ${city}. I enjoy ${pasttime} and coding!`;
+console.log(myStory);
